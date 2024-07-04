9c1f347acc5996a0a8da692160efb01f6e4e354a
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-07-05 05:29:35 +0530

    🆕Feat: chatid using param.

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -3,9 +3,13 @@ import Header from "./Header";
 import Title from "../shared/Title";
 import { Grid } from "@mui/material";
 import ChatList from "../specific/ChatList";
+import { samepleChats } from "../../constants/sampleData";
+import { useParams } from "react-router-dom";
 
 const AppLayout = (WrappedComponent) => {
   return (props) => {
+    const params = useParams();
+    const chatId = params.chatId;
     return (
       <>
         <Title />
@@ -21,18 +25,41 @@ const AppLayout = (WrappedComponent) => {
             height={"100%"}
             bgcolor="primary.main"
           >
-            <ChatList/>
+            <ChatList
+              chats={samepleChats}
+              chatId={chatId}
+              newMessagesAlert={[
+                {
+                  chatId: "1",
+                  count: "4",
+                },
+              ]}
+              onlineUsers={["1", "2"]}
+            />
           </Grid>
-          <Grid item xs={12}  sm={8} md={5} lg={6} height={"100%"} bgcolor="primary.main">
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={5}
+            lg={6}
+            height={"100%"}
+            bgcolor="primary.main"
+          >
             <WrappedComponent {...props} />
           </Grid>
-          <Grid item  md={4} lg={3} height={"100%"} 
-          sx={{
-            display: { xs: "none", sm: "block" },
-            padding:'2rem',
-            bgcolor:'rgba(0,0,0,0.85)'
-          }}
-          bgcolor="primary.main">
+          <Grid
+            item
+            md={4}
+            lg={3}
+            height={"100%"}
+            sx={{
+              display: { xs: "none", sm: "block" },
+              padding: "2rem",
+              bgcolor: "rgba(0,0,0,0.85)",
+            }}
+            bgcolor="primary.main"
+          >
             Third
           </Grid>
         </Grid>
