fee97350381873a44a0e317cb973e10d65a9823a
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-07-05 01:03:50 +0530

    🆕Feat: Setup the materialUi.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </React.StrictMode>
