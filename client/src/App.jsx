043dae8b70dd1d72a17a5da85a333fc8462d8552
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-07-05 10:06:04 +0530

    🆕Feat: Check cookies if already login then user do not login again.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import { LayoutLoader } from "./components/layout/Loader";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { server } from "./constants/config";
+import { userExists, userNotExists } from "./redux/reducers/auth";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -9,10 +13,20 @@ const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
-let user = true;
-
 function App() {
-  return (
+  const { user, loader } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get(`${server}/api/v1/user/me`, { withCredentials: true })
+      .then(({ data }) => dispatch(userExists(data.user)))
+      .catch((err) => dispatch(userNotExists()));
+  }, [dispatch]);
+
+  return loader ? (
+    <LayoutLoader />
+  ) : (
     <BrowserRouter>
       <Suspense fallback={<LayoutLoader />}>
         <Routes>
