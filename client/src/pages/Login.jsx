0088501e9792dbda7c0c9733b27311e5d911d5e6
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-07-05 10:56:43 +0530

    🆕Feat:User login signup completed .

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,70 +8,161 @@ import {
   Stack,
   TextField,
   Typography,
-  Grid,
-  useMediaQuery,
 } from "@mui/material";
-import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as yup from "yup";
-import { useTheme } from "@mui/material/styles";
-import { bgGradient } from "../constants/color.jsx";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
 import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
-
-const validationSchema = yup.object({
-  name: yup.string("Enter your name").required("Name is required"),
-  bio: yup.string("Enter your bio").required("Bio is required"),
-  username: yup.string("Enter your username").required("Username is required"),
-  password: yup
-    .string("Enter your password")
-    .min(8, "Password should be of minimum 8 characters length")
-    .required("Password is required"),
-});
+import { bgGradient } from "../constants/color";
+import { server } from "../constants/config";
+import { userExists } from "../redux/reducers/auth";
 
 const Login = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  const [avatar, setAvatar] = useState();
+  const [avatarError, setAvatarError] = useState("");
+  const [errors, setErrors] = useState({});
 
-  const toggleLogin = () => setIsLogin((prev) => !prev);
+  const [userData, setUserData] = useState({
+    name: "",
+    bio: "",
+    username: "",
+    password: "",
+    avatar: null,
+  });
 
-  const handleAvatarChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setAvatar(file);
-    }
-  };
-
-  const formik = useFormik({
-    initialValues: {
+  const toggleLogin = () => {
+    setIsLogin((prev) => !prev);
+    setUserData({
       name: "",
       bio: "",
       username: "",
       password: "",
-    },
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      setIsLoading(true);
-      if (isLogin) {
-        // Handle login logic
-        await handleLogin(values);
-      } else {
-        // Handle signup logic
-        await handleSignUp(values);
-      }
+      avatar: null,
+    });
+    setErrors({});
+    setAvatarError("");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleAvatarChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUserData((prevData) => ({
+        ...prevData,
+        avatar: file,
+      }));
+      setAvatarError("");
+    } else {
+      setAvatarError("Please select a valid image file.");
+    }
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!userData.username) newErrors.username = "Username is required.";
+    if (!userData.password) newErrors.password = "Password is required.";
+    if (!isLogin) {
+      if (!userData.name) newErrors.name = "Name is required.";
+      if (!userData.bio) newErrors.bio = "Bio is required.";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+    const toastId = toast.loading("Logging In...");
+
+    setIsLoading(true);
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/user/login`,
+        {
+          username: userData.username,
+          password: userData.password,
+        },
+        config
+      );
+      dispatch(userExists(data.user));
+      toast.success(data.message, {
+        id: toastId,
+      });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something Went Wrong", {
+        id: toastId,
+      });
+    } finally {
       setIsLoading(false);
-    },
-  });
-
-  const handleLogin = async (values) => {
-    // Implement login logic
+    }
   };
 
-  const handleSignUp = async (values) => {
-    // Implement signup logic
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    const toastId = toast.loading("Signing Up...");
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append("avatar", userData.avatar);
+    formData.append("name", userData.name);
+    formData.append("bio", userData.bio);
+    formData.append("username", userData.username);
+    formData.append("password", userData.password);
+
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/user/new`,
+        formData,
+        config
+      );
+
+      dispatch(userExists(data.user));
+      toast.success(data.message, {
+        id: toastId,
+      });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something Went Wrong", {
+        id: toastId,
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {
+    return () => {
+      if (userData.avatar) {
+        URL.revokeObjectURL(userData.avatar);
+      }
+    };
+  }, [userData.avatar]);
 
   return (
     <div
@@ -80,14 +171,13 @@ const Login = () => {
       }}
     >
       <Container
+        component={"main"}
+        maxWidth="xs"
         sx={{
           height: "100vh",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-        }}
-        style={{
-          width: isSmallScreen ? "400px" : "600px",
         }}
       >
         <Paper
@@ -107,7 +197,7 @@ const Login = () => {
                   width: "100%",
                   marginTop: "1rem",
                 }}
-                onSubmit={formik.handleSubmit}
+                onSubmit={handleLogin}
               >
                 <TextField
                   required
@@ -115,14 +205,11 @@ const Login = () => {
                   label="Username"
                   margin="normal"
                   variant="outlined"
-                  id="username"
                   name="username"
-                  value={formik.values.username}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.username && Boolean(formik.errors.username)
-                  }
-                  helperText={formik.touched.username && formik.errors.username}
+                  value={userData.username}
+                  onChange={handleInputChange}
+                  error={!!errors.username}
+                  helperText={errors.username}
                 />
 
                 <TextField
@@ -132,14 +219,11 @@ const Login = () => {
                   type="password"
                   margin="normal"
                   variant="outlined"
-                  id="password"
                   name="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                  }
-                  helperText={formik.touched.password && formik.errors.password}
+                  value={userData.password}
+                  onChange={handleInputChange}
+                  error={!!errors.password}
+                  helperText={errors.password}
                 />
 
                 <Button
@@ -171,158 +255,139 @@ const Login = () => {
             </>
           ) : (
             <>
-              <Container
-                maxWidth={false}
+              <Typography variant="h5">Sign Up</Typography>
+              <form
                 style={{
-                  width: isSmallScreen ? "400px" : "600px",
-                  maxWidth: isSmallScreen ? "400px" : "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  width: "100%",
+                  marginTop: "1rem",
                 }}
+                onSubmit={handleSignUp}
               >
-                <Typography variant="h5">Sign Up</Typography>
-                <form
-                  style={{
-                    width: "100%",
+                <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                  <Avatar
+                    sx={{
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                    src={
+                      userData.avatar ? URL.createObjectURL(userData.avatar) : ""
+                    }
+                  />
+
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "white",
+                      bgcolor: "rgba(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgba(0,0,0,0.7)",
+                      },
+                    }}
+                    component="label"
+                  >
+                    <>
+                      <CameraAltIcon />
+                      <VisuallyHiddenInput
+                        type="file"
+                        onChange={handleAvatarChange}
+                      />
+                    </>
+                  </IconButton>
+                </Stack>
+
+                {avatarError && (
+                  <Typography
+                    m="1rem auto"
+                    width="fit-content"
+                    display="block"
+                    color="error"
+                    variant="caption"
+                  >
+                    {avatarError}
+                  </Typography>
+                )}
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Name"
+                  margin="normal"
+                  variant="outlined"
+                  name="name"
+                  value={userData.name}
+                  onChange={handleInputChange}
+                  error={!!errors.name}
+                  helperText={errors.name}
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Bio"
+                  margin="normal"
+                  variant="outlined"
+                  name="bio"
+                  value={userData.bio}
+                  onChange={handleInputChange}
+                  error={!!errors.bio}
+                  helperText={errors.bio}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                  name="username"
+                  value={userData.username}
+                  onChange={handleInputChange}
+                  error={!!errors.username}
+                  helperText={errors.username}
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleInputChange}
+                  error={!!errors.password}
+                  helperText={errors.password}
+                />
+
+                <Button
+                  sx={{
                     marginTop: "1rem",
                   }}
-                  onSubmit={formik.handleSubmit}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                  disabled={isLoading}
                 >
-                  <Grid container spacing={2} justifyContent="center">
-                    <Grid item xs={12} md={6}>
-                      <Stack
-                        position={"relative"}
-                        width={"10rem"}
-                        margin={"auto"}
-                      >
-                        <Avatar
-                          sx={{
-                            width: "10rem",
-                            height: "10rem",
-                            objectFit: "contain",
-                          }}
-                          src={avatar ? URL.createObjectURL(avatar) : ""}
-                        />
-                        <IconButton
-                          sx={{
-                            position: "absolute",
-                            bottom: "0",
-                            right: "0",
-                            color: "white",
-                            bgcolor: "rgba(0,0,0,0.5)",
-                            ":hover": {
-                              bgcolor: "rgba(0,0,0,0.7)",
-                            },
-                          }}
-                          component="label"
-                        >
-                          <>
-                            <CameraAltIcon />
-                            <VisuallyHiddenInput
-                              type="file"
-                              onChange={handleAvatarChange}
-                            />
-                          </>
-                        </IconButton>
-                      </Stack>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                      <TextField
-                        required
-                        fullWidth
-                        label="Name"
-                        margin="normal"
-                        variant="outlined"
-                        id="name"
-                        name="name"
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.name && Boolean(formik.errors.name)
-                        }
-                        helperText={formik.touched.name && formik.errors.name}
-                      />
-                      <TextField
-                        required
-                        fullWidth
-                        label="Bio"
-                        margin="normal"
-                        variant="outlined"
-                        id="bio"
-                        name="bio"
-                        value={formik.values.bio}
-                        onChange={formik.handleChange}
-                        error={formik.touched.bio && Boolean(formik.errors.bio)}
-                        helperText={formik.touched.bio && formik.errors.bio}
-                      />
-                      <TextField
-                        required
-                        fullWidth
-                        label="Username"
-                        margin="normal"
-                        variant="outlined"
-                        id="username"
-                        name="username"
-                        value={formik.values.username}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.username &&
-                          Boolean(formik.errors.username)
-                        }
-                        helperText={
-                          formik.touched.username && formik.errors.username
-                        }
-                      />
-                      <TextField
-                        required
-                        fullWidth
-                        label="Password"
-                        type="password"
-                        margin="normal"
-                        variant="outlined"
-                        id="password"
-                        name="password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.password &&
-                          Boolean(formik.errors.password)
-                        }
-                        helperText={
-                          formik.touched.password && formik.errors.password
-                        }
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Button
-                        sx={{
-                          marginTop: "1rem",
-                        }}
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                        fullWidth
-                        disabled={isLoading}
-                      >
-                        Sign Up
-                      </Button>
-                      <Typography textAlign={"center"} m={"1rem"}>
-                        OR
-                      </Typography>
-                      <Button
-                        disabled={isLoading}
-                        fullWidth
-                        variant="text"
-                        onClick={toggleLogin}
-                      >
-                        Login Instead
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </form>
-              </Container>
+                  Sign Up
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+
+                <Button
+                  disabled={isLoading}
+                  fullWidth
+                  variant="text"
+                  onClick={toggleLogin}
+                >
+                  Login Instead
+                </Button>
+              </form>
             </>
           )}
         </Paper>
