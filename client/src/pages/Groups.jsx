037b92b5a77787deea1493e465634d28d48b57c3
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-07-12 05:08:20 +0530

    🆕Feat Under maintenance.

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Error as ErrorIcon } from "@mui/icons-material";
+import { Container, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function Groups() {
+const Groups = () => {
   return (
-    <div>
-      Groups
-    </div>
-  )
-}
+    <Container maxWidth="lg" sx={{ height: "100vh" }}>
+      <Stack
+        alignItems={"center"}
+        spacing={"2rem"}
+        justifyContent={"center"}
+        height="100%"
+      >
+        <ErrorIcon sx={{ fontSize: "10rem" }} />
+        <Typography variant="h1">503</Typography>
+        <Typography variant="h3">Under Maintenance</Typography>
+        <Link to="/">Go back to home</Link>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Groups;
